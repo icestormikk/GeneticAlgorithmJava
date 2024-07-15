d06d99b6586bd0515021bdea18b46eecd9f45d03
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-15 10:46:35 +0300

    Добавил конструктор с ObservableList

--- src/main/java/org/icestormikk/geneticalgorithmjava/core/domain/Chromosome.java
+++ src/main/java/org/icestormikk/geneticalgorithmjava/core/domain/Chromosome.java
@@ -1,5 +1,8 @@
 package org.icestormikk.geneticalgorithmjava.core.domain;
 
+import javafx.collections.ObservableList;
+import org.icestormikk.geneticalgorithmjava.core.domain.abstraction.AbstractChromosome;
+
 import java.util.List;
 
 /**
@@ -8,6 +11,9 @@ import java.util.List;
  */
 public class Chromosome<T> extends AbstractChromosome<T> {
     public Chromosome(List<T> genes) {
+        super(genes);
+    }
+    public Chromosome(ObservableList<T> genes) {
         super(genes);
     }
 }
