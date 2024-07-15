400fadca42de51de2e1cdaf71bc5ea7eab982e6e
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-15 10:50:20 +0300

    Общие обновления файлов

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,7 @@ module org.icestormikk.geneticalgorithmjava {
     requires org.kordamp.bootstrapfx.core;
     requires static lombok;
 
-    opens org.icestormikk.geneticalgorithmjava to javafx.fxml;
+    opens org.icestormikk.geneticalgorithmjava to javafx.fxml,javafx.controls;
     exports org.icestormikk.geneticalgorithmjava;
     exports org.icestormikk.geneticalgorithmjava.gui.contollers;
     opens org.icestormikk.geneticalgorithmjava.gui.contollers to javafx.fxml;
