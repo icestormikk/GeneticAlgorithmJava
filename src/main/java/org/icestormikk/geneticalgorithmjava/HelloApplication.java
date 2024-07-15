6b8da2deca80938bb06a19b330bf87a01780172b
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-15 10:49:56 +0300

    Немного тестового кода в главном методе

--- src/main/java/org/icestormikk/geneticalgorithmjava/HelloApplication.java
+++ src/main/java/org/icestormikk/geneticalgorithmjava/HelloApplication.java
@@ -4,8 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.icestormikk.geneticalgorithmjava.core.domain.Chromosome;
+import org.icestormikk.geneticalgorithmjava.core.domain.GeneticAlgorithm;
+import org.icestormikk.geneticalgorithmjava.core.domain.Population;
+import org.icestormikk.geneticalgorithmjava.core.domain.operators.parents.RouletteWheelParentsSelector;
 
 import java.io.IOException;
+import java.util.Random;
+import java.util.stream.Stream;
 
 public class HelloApplication extends Application {
     @Override
@@ -18,6 +24,19 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        Population<Integer, Chromosome<Integer>> population = new Population<>(
+            Stream.generate(() -> new Chromosome<>(
+                Stream.generate(() -> new Random().nextInt(0, 10)).limit(10).toList()
+            ))
+            .limit(10)
+            .peek((c) -> c.setFitness(new Random().nextDouble(0, 100)))
+            .toList()
+        );
+
+        GeneticAlgorithm<Integer, Chromosome<Integer>, Population<Integer, Chromosome<Integer>>> ga = new GeneticAlgorithm<>();
+        ga.setParentsSelector(new RouletteWheelParentsSelector<>(2));
+        System.out.println(ga.applyParentsSelection(population));
+
         launch();
     }
 }
